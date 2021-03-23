841501ba4149df4a9f45368913277ff9128c2afb
jtwoolbright <woolbright.josh.t@gmail.com>
2021-03-23 14:40:43 -0700

    Got rid of nested linear layouts

--- app/src/main/java/com/example/covidriskassessment/MainAdapter.kt
+++ app/src/main/java/com/example/covidriskassessment/MainAdapter.kt
@@ -5,14 +5,14 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import androidx.recyclerview.widget.RecyclerView
-import kotlinx.android.synthetic.main.recyclerviewlayout.view.*
+import kotlinx.android.synthetic.main.row_item.view.*
 
 class MainAdapter:RecyclerView.Adapter<CustomViewHolder>(){
-    val Questions = listOf<String>("Asthma?","Cardiovascular Disease?","Chronic Lung Disease?","Immune Suppression?","Metabolic Disease?","Neurologic Disease?","Auto-Immune Disease?","Liver Disease?","Obesity?","Pregnancy?","Obestiy","Pregnancy","Hypertension?")
+    val Questions = listOf<String>("Asthma?","Cardiovascular Disease?","Chronic Lung Disease?","Immune Suppression?","Metabolic Disease?","Neurologic Disease?","Auto-Immune Disease?","Liver Disease?","Obesity?","Pregnancy?","Hypertension?")
     val Options = listOf<String>()
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CustomViewHolder {
         val layoutInflater = LayoutInflater.from(parent?.context)
-        val cellForRow = layoutInflater.inflate(R.layout.recyclerviewlayout,parent,false)
+        val cellForRow = layoutInflater.inflate(R.layout.row_item,parent,false)
         return CustomViewHolder(cellForRow)
     }
     // # of items
